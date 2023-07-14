5e427106b09f0d4200b32657a8f0ebd5cb002d0c
lookeey <71905281+lookeey@users.noreply.github.com>
2023-07-14 00:19:59 -0300

    fix missing dep

--- interface/components/input/BalanceInput.tsx
+++ interface/components/input/BalanceInput.tsx
@@ -1,4 +1,3 @@
-import { BigNumber } from "ethers";
 import {
   Button,
   Flex,
--- interface/components/input/BigIntSlider.tsx
+++ interface/components/input/BigIntSlider.tsx
@@ -1,4 +1,4 @@
-import { formatUnits } from "ethers/lib/utils";
+import { formatUnits } from "@ethersproject/units";
 import {
     Slider,
     SliderFilledTrack,
